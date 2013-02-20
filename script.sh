6e53bd9546ee970466e02b391da15aaf46bdb3ce
Youngbin Kim <yb.kim@yonsei.ac.kr>
2013-02-20 16:13:58 +0900

    Initial Commit

---
+++ script.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+compiler=arm-linux-gnueabi-gcc
+
+case $1 in
+
+	"download")
+	for file in automotive consumer network office security telecomm
+	do
+		wget www.eecs.umich.edu/mibench/$file.tar.gz
+	done
+	;;
+
+  "make")
+  find . -name "Makefile" -exec perl -pi -e 's/(\s*)gcc/\g1arm-linux-gnueabi-gcc/g' {} \;
+  
+  for dir in automotive consumer network office security telecomm
+  do
+    cd $dir
+    d=$(pwd)
+    for bench in $(ls)
+    do
+      echo "compiling $bench"
+      cd $bench
+      make
+      make all
+      cd $d
+    done
+    cd ..
+  done
+  ;;
+
+esac
